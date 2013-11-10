c2cae6bd26ba04efa1c3b33cba856291c9e32060
Christopher Schmidt <christopher.schmidt@here.com>
2013-11-10 08:32:09 -0500

    Set comments to 0.
    Since comments are usually 0, just force them to that value; this
    improves rmlse by about .08. On the test set, this moves my score
    up by 0.07956 to 0.81196.

--- naive_average.py
+++ naive_average.py
@@ -20,12 +20,12 @@ outf.write("id,num_views,num_votes,num_comments\n")
 avg_views_ipart = int(avg_views)
 avg_comments_ipart = int(avg_comments)
 avg_votes_ipart = int(avg_votes)
-r = csv.reader(open("train.csv"))
+r = csv.reader(open("test.csv"))
 r.next()
 for row in r:
     id = row[0]
     num_views = avg_views_ipart + 1 * (random.random() > (avg_views - avg_views_ipart))
     num_votes = avg_votes_ipart + 1 * (random.random() > (avg_votes - avg_votes_ipart))
-    num_comments = avg_comments_ipart + 1 * (random.random() > (avg_comments - avg_comments_ipart))
+    num_comments = 0 # avg_comments_ipart + 1 * (random.random() > (avg_comments - avg_comments_ipart))
     print >>outf, "%i,%i,%i,%i" % (int(id), num_views, num_votes, num_comments)
 

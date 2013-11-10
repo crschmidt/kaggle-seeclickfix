c731ada3aeb8aaf5b39528287b8bed517c6d667e
Christopher Schmidt <christopher.schmidt@here.com>
2013-11-10 08:34:57 -0500

    Add debug information from rmsle output.
    Print rowdata that shows how far off the result is from truth
    as a CSV file.

--- rmsle.py
+++ rmsle.py
@@ -12,12 +12,14 @@ r.next()
 votes = comments = views = 0
 total_sum = 0
 count = 0
+rowfile = open("rowfile.csv", "w")
 for row in r:
 #    print row
     test = d[row[0]]
     votes, comments, views = map(int, row[5:8])
     test_views, test_votes, test_comments = map(int, test[1:])
     row_sum = sum([(math.log(test_views+1) - math.log(views+1))**2, (math.log(test_votes+1) - math.log(votes+1))**2, (math.log(test_comments+1)-math.log(comments+1))**2])
+    print >> rowfile, "%s,%s" % (row[0],row_sum) 
     total_sum += row_sum
     count += 1
 print float(1)/(count*3) * total_sum    
